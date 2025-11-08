fb494b8d30208e96624d4055e127b20ce8d8aecd
Nazar_O <nazar.orlovskuy49@gmail.com>
2025-11-07 23:30:40 -0600

    fiveth

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './components/App/App';
 import 'modern-normalize/modern-normalize.css';
-import '../src/components/App/App.module.css';
+// import '../src/components/App/App.module.css';
 import { Toaster } from 'react-hot-toast';
 
 const root = createRoot(document.getElementById('root')!);
